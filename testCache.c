f94b61a99e2b123108a31afa8ed1cc0ff6200e4d
Kasper Seglem <kasperseglem@engr2-6-185-dhcp.int.colorado.edu>
2023-02-14 09:49:25 -0700

    cycles

--- testCache.c
+++ testCache.c
@@ -15,13 +15,49 @@
 
 #define CACHE_SIZE (1024 * 1024) // 1MB
 
+#define N (1024*4)
+
+unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
+
+static __inline__ unsigned long long rdtsc(void)
+{
+    __asm__ __volatile__ ("RDTSC\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+            "%rax", "rbx", "rcx", "rdx");
+}
+
+static __inline__ unsigned long long rdtsc1(void)
+{
+    __asm__ __volatile__ ("RDTSC\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t": "=r" (cycles_high1), "=r" (cycles_low1)::
+            "%rax", "rbx", "rcx", "rdx");
+}
+
+int main(int argc, char* argv[])
+{
+    uint64_t start, end;
+
+    rdtsc();
+    malloc(N);
+    rdtsc1();
+
+    start = ( ((uint64_t)cycles_high << 32) | cycles_low );
+    end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
+
+    printf("cycles spent in allocating %d bytes of memory: %llu\n",N, end - start);
+
+    return 0;
+}
+/*
 int main() {
 
 	printf("start\n");
 	int num = 5;
 	printf("start\n");
 	int a[CACHE_SIZE]; 
-
+*/
 //	char *cache = malloc(CACHE_SIZE);
 
 //	if (cache == NULL) {
@@ -47,7 +83,7 @@ int main() {
 	printf("Cache filled successfully\n");
 */
 //	free(cache);
-
+/*
 	usleep(10);
 
 	int z;
@@ -65,4 +101,4 @@ int main() {
 	printf("time spent: %f\n",time_spent);
 
 	return 0;
-}
+}*/
