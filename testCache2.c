21befc3ed82b1f1b8187cdbf17486bb627c8a763
kase1828 <kase1828@colorado.edu>
2023-03-01 10:26:30 -0700

    a1 kidus help

--- testCache2.c
+++ testCache2.c
@@ -14,45 +14,114 @@ int main() {
 
         printf("start\n");
 
-	u_int64_t a[CACHE_SIZE / 32];
-	u_int64_t b[CACHE_SIZE / 8];
-	u_int64_t a1[CACHE_SIZE / 32];
-	u_int64_t a2[CACHE_SIZE / 32];
+	u_int64_t a[1024 * 1024 / 8];
+	u_int64_t b[1024 * 1024 / 8];
+	u_int64_t a1[1024 * 1024 / 8];
+	u_int64_t a2[1024 * 1024 / 8];
+	u_int64_t a3[1024 * 1024 / 8];
+	u_int64_t b1[1024 * 1024 / 8];
 	u_int64_t num;
 
 	clock_t begin;
 	clock_t end;
 	double time_spent;
 
-	for (u_int64_t i = 0; i < CACHE_SIZE / 32; i++) {
+	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 a[i] = rand();
+        }
+
+	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                a1[i] = a[i];
+		//num = a[i];
         }
 
 	begin = clock();
 
-	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
-                a1[i] = a[i];
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                num = a1[i];
         }
 
         end = clock();
         time_spent = (double)(end - begin);
 
-	printf("a1 time: %f\n",time_spent);
+        printf("a1 read: %f\n",time_spent);	
 
+	printf("%d\n",num);
+
+	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                a2[i] = a[i];
+		//num = a[i];
+        }
+
+	begin = clock();
+
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                num = a1[i];
+        }
+
+        end = clock();
+        time_spent = (double)(end - begin);
+
+        printf("a1 read after a2: %f\n",time_spent);	
+
+	printf("%d\n",num);
+
+	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                a2[i] = a[i];
+                //num = a[i];
+        }
+
+	begin = clock();
+
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                num = a2[i];
+        }
+
+        end = clock();
+        time_spent = (double)(end - begin);
+
+        printf("a2 read: %f\n",time_spent);
+	
 	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 b[i] = rand();
         }
 
 	begin = clock();
 
-	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
-                a2[i] = a[i];
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                num = a2[i];
         }
 
         end = clock();
         time_spent = (double)(end - begin);
 
-	printf("a2 time: %f\n",time_spent);
+        printf("a2 read after b: %f\n",time_spent);
+	printf("%d\n",num);
+	
+/*
+	begin = clock();
 
+	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                b1[i] = b[i];
+		//num = b[i];
+        }
+
+        end = clock();
+        time_spent = (double)(end - begin);
+
+	printf("b1 time: %f\n",time_spent);
+
+	begin = clock();
+
+	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                a3[i] = a[i];
+		//num = a[i];
+        }
+
+        end = clock();
+        time_spent = (double)(end - begin);
+
+	printf("a3 time: %f\n",time_spent);
+*/
 	return 0;
 }
