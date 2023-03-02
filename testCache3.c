6018c48553736bcdb1d0390d4fb85b1b94cd2ef6
Kasper Seglem <kasperseglem@engr2-6-105-dhcp.int.colorado.edu>
2023-03-02 14:43:35 -0700

    test 3

--- testCache3.c
+++ testCache3.c
@@ -8,38 +8,38 @@
 #include <errno.h>
 #define _GNU_SOURCE
 
-#define CACHE_SIZE (1024 * 1024) // 1MB
+#define CACHE_SIZE (16384) // 1MB
 
 int main() {
 
         printf("start\n");
 
-	u_int64_t a[1024 * 1024 / 8];
-	u_int64_t b[1024 * 1024 / 8];
-	u_int64_t a1[1024 * 1024 / 8];
-	u_int64_t a2[1024 * 1024 / 8];
-	u_int64_t a3[1024 * 1024 / 8];
-	u_int64_t b1[1024 * 1024 / 8];
-	u_int64_t num;
+        u_int64_t a[16384 / 8];
+        u_int64_t b[16384 / 8];
+        u_int64_t a1[16384 / 8];
+        u_int64_t a2[16384 / 8];
+        u_int64_t a3[16384 / 8];
+        u_int64_t b1[16384 / 8];
+        u_int64_t num;
 
-	clock_t begin;
-	clock_t end;
-	double time_spent;
+        clock_t begin;
+        clock_t end;
+        double time_spent;
 
-	for (u_int64_t i = 0; i < 16384 / 8; i++) {
+        for (u_int64_t i = 0; i < 16384 / 8; i++) {
                 c[i] = rand();
         }
 
-	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 a[i] = rand();
         }
 
-	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 a1[i] = a[i];
-		//num = a[i];
+                //num = a[i];
         }
 
-	begin = clock();
+        begin = clock();
 
         for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 num = a1[i];
@@ -48,34 +48,34 @@ int main() {
         end = clock();
         time_spent = (double)(end - begin);
 
-        printf("a1 read: %f\n",time_spent);	
+        printf("a1 read: %f\n",time_spent);
 
-	printf("%d\n",num);
-
-	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
-                a2[i] = a[i];
-		//num = a[i];
-        }
-
-	begin = clock();
+        printf("%d\n",num);
 
         for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
-                num = a1[i];
-        }
-
-        end = clock();
-        time_spent = (double)(end - begin);
-
-        printf("a1 read after a2: %f\n",time_spent);	
-
-	printf("%d\n",num);
-
-	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 a2[i] = a[i];
                 //num = a[i];
         }
 
-	begin = clock();
+        begin = clock();
+
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                num = a1[i];
+        }
+
+        end = clock();
+        time_spent = (double)(end - begin);
+
+        printf("a1 read after a2: %f\n",time_spent);
+
+        printf("%d\n",num);
+
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+                a2[i] = a[i];
+                //num = a[i];
+        }
+
+        begin = clock();
 
         for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 num = a2[i];
@@ -85,12 +85,12 @@ int main() {
         time_spent = (double)(end - begin);
 
         printf("a2 read: %f\n",time_spent);
-	
-	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 b[i] = rand();
         }
 
-	begin = clock();
+        begin = clock();
 
         for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 num = a2[i];
@@ -100,32 +100,32 @@ int main() {
         time_spent = (double)(end - begin);
 
         printf("a2 read after b: %f\n",time_spent);
-	printf("%d\n",num);
-	
+        printf("%d\n",num);
+
 /*
-	begin = clock();
+        begin = clock();
 
-	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 b1[i] = b[i];
-		//num = b[i];
+                //num = b[i];
         }
 
         end = clock();
         time_spent = (double)(end - begin);
 
-	printf("b1 time: %f\n",time_spent);
+        printf("b1 time: %f\n",time_spent);
 
-	begin = clock();
+        begin = clock();
 
-	for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
+        for (u_int64_t i = 0; i < CACHE_SIZE / 8; i++) {
                 a3[i] = a[i];
-		//num = a[i];
+                //num = a[i];
         }
 
         end = clock();
         time_spent = (double)(end - begin);
 
-	printf("a3 time: %f\n",time_spent);
+        printf("a3 time: %f\n",time_spent);
 */
-	return 0;
+        return 0;
 }
